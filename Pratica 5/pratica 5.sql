cadd0284bba27a2115fe654a04dbde8558c464b9
ryangsantos2 <160976028+ryangsantos2@users.noreply.github.com>
2024-04-11 22:59:41 -0300

    Update pratica 5.sql

--- Pratica 5/pratica 5.sql
+++ Pratica 5/pratica 5.sql
@@ -26,6 +26,7 @@ fkCliente int,
 nome varchar(45),
 raca varchar(45),
 dtNasc date,
+tipo varchar(45),
 	constraint cliente_pet
     foreign key (fkCliente)
     references cliente(idCliente)
@@ -34,19 +35,13 @@ dtNasc date,
    
 alter table pet auto_increment = 101;
 
-select * from pet;
-
-
-
-
-
 insert into pet values
 
-(101,'1','Nina', 'Shin tzu', '2017-08-09', 'cachorro'),
-(101,'2', 'Luke', 'Shin tzu', '2013-04-04', 'cachorro'),
-(102, '2', 'Mel', 'pug', '2015-10-15', 'gato'),
-(101, '3', 'Chico', 'pug', '2018-04-10', 'cachorro'),
-(102, '1', 'Bolt', 'pit bull', '2020-08-08', 'gato');
+(default,'1','Nina', 'Shin tzu', '2017-08-09', 'cachorro'),
+(default,'2', 'Luke', 'Shin tzu', '2013-04-04', 'cachorro'),
+(default, '2', 'Mel', 'pug', '2015-10-15', 'gato'),
+(default, '3', 'Chico', 'pug', '2018-04-10', 'cachorro'),
+(default, '1', 'Bolt', 'pit bull', '2020-08-08', 'gato');
 
 select * from pet where tipo = 'gato';
 select nome, dtNasc from pet;
@@ -76,3 +71,65 @@ select *
     
 drop table pet; 
 drop table cliente;
+
+												-- ATIVIDADE 2
+  
+CREATE TABLE pessoa (
+  idPessoa INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(45) ,
+  dtNasc VARCHAR(45) ,
+  profissao VARCHAR(45)
+  );
+
+
+insert into pessoa values
+(default,'Ryan', '2005-10-19', 'Desenvolvedor backend'),
+(default,'Julia', '2005-06-27', 'Profissional em estética capilar'),
+(default,'Vinicius', '2004-09-19', 'Biologo'),
+(default,'Rayssa', '2010-09-10', 'Medica veterinaria');
+
+
+CREATE TABLE gasto (
+  idGasto INT AUTO_INCREMENT,
+  fkPessoa INT,
+	CONSTRAINT pkPessoaGasto
+    PRIMARY KEY (idGasto, fkPessoa),
+	
+  data DATE,
+  valor VARCHAR(25),
+  descricao VARCHAR(45),
+	CONSTRAINT gasto_pessoa 
+    FOREIGN KEY(fkPessoa)
+    REFERENCES pessoa(idPessoa)
+  );
+  
+  INSERT INTO gasto VALUES
+  (default,2 ,'2024-04-05', '100,00', 'tesoura'),
+  (default, 1,'2024-10-19', '2750,00', 'notebook'),
+  (default, 2,'2024-12-22', '529,99', 'escova'),
+  (default, 3,'2024-05-05', '100,00', 'jaleco profissional'),
+  (default, 1,'2024-08-08','279,99', 'memoria ram'),
+  (default, 3,'2024-09-29', '1999,99', 'centrifuga'),
+  (default, 4,'2024-05-11', '49,99', 'kit luvas medicas'),
+  (default, 4,'2024-06-20', '100,00', 'jaleco prossional');
+ 
+ select * from pessoa;
+ select * from gasto;
+ 
+  select * from pessoa where profissao = 'biologo';
+ select * from gasto where descricao = 'escova';
+ 
+ select * from pessoa 
+ join gasto
+ on pessoa.idPessoa = gasto.fkPessoa;
+ 
+ select pessoa.nome as NOME, gasto.descricao as GASTO
+	from pessoa 
+    join gasto
+    on pessoa.idPessoa=gasto.fkPessoa
+    where nome = 'Ryan';
+    
+    update pessoa set profissao = 'profissional em bando de dados' where idPessoa = 1;
+    delete from gasto where idGasto = 1;
+    
+						-- ATIVIDADE 3
