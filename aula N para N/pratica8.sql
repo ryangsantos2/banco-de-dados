4bb830b7c32e47ae58fb4c2436b1dff2848247cb
ryangsantos2 <160976028+ryangsantos2@users.noreply.github.com>
2024-05-03 14:40:36 -0300

    Update pratica8.sql
    pratica 8 concluida

--- aula N para N/pratica8.sql
+++ aula N para N/pratica8.sql
@@ -59,6 +59,7 @@ INSERT INTO venda VALUES
 SELECT * FROM cliente;
 SELECT * FROM produto;
 SELECT * FROM venda;
+
 -- g) Exibir os dados dos clientes e os dados de suas respectivas vendas.
 
 SELECT cliente.nome, produto.nome, venda.dtVenda
@@ -66,7 +67,88 @@ FROM cliente
 JOIN venda
 	ON cliente.idCliente = venda.fkCliente
 JOIN produto
-	ON produto.idProduto = venda.fkProduto
+	ON produto.idProduto = venda.fkProduto;
+
 -- h) Exibir os dados de um determinado cliente (informar o nome do cliente na consulta) e os dados de suas respectivas vendas.
+SELECT cliente.nome, produto.nome, venda.dtVenda
+FROM cliente 
+JOIN venda
+	ON cliente.idCliente = venda.fkCliente
+JOIN produto
+	ON produto.idProduto = venda.fkProduto
+WHERE cliente.nome = 'Woody';
 
 -- i) Exibir os dados dos clientes e de suas respectivas indicações de clientes
+SELECT cliente.nome AS CLIENTE, indicado.nome AS INDICADO
+	FROM cliente
+    JOIN cliente AS indicado
+		ON cliente.idCliente = indicado.fkIndicado;
+        
+-- j) Exibir os dados dos clientes indicados e os dados dos respectivos clientes
+-- indicadores, porém somente de um determinado cliente indicador (informar o nome
+-- do cliente que indicou na consulta).
+SELECT cliente.nome AS CLIENTE, indicado.nome AS INDICADO
+	FROM cliente
+    JOIN cliente AS indicado
+		ON cliente.idCliente = indicado.fkIndicado
+			WHERE cliente.nome = 'Picapau';
+
+-- l) Exibir os dados dos clientes, os dados dos respectivos clientes que indicaram, os
+-- dados das respectivas vendas e dos produtos.
+SELECT *
+FROM cliente 
+JOIN venda
+	ON cliente.idCliente = venda.fkCliente
+JOIN produto
+	ON produto.idProduto = venda.fkProduto
+ JOIN cliente AS indicador
+	ON cliente.idCliente = indicador.fkIndicado;
+
+-- m) Exibir apenas a data da venda, o nome do produto e a quantidade do produto
+-- numa determinada venda.
+SELECT produto.nome,data.dtVenda, qtd.qtdVenda
+	FROM produto
+    JOIN venda AS data
+		ON produto.idProduto = data.fkProduto
+	JOIN venda AS qtd
+		ON produto.idProduto = qtd.fkProduto;
+        
+-- n) Exibir apenas o nome do produto, o valor do produto e a soma da quantidade de
+-- produtos vendidos agrupados pelo nome do produto.
+SELECT pro;
+
+-- o) Inserir dados de um novo cliente. Exibir os dados dos clientes, das respectivas
+-- vendas, e os clientes que não realizaram nenhuma venda.
+
+-- p) Exibir o valor mínimo e o valor máximo dos preços dos produtos;
+SELECT MAX(preco) FROM produto;
+SELECT MIN(preco) FROM produto;
+
+-- q) Exibir a soma e a média dos preços dos produtos;
+SELECT SUM(preco) FROM produto;
+SELECT AVG(preco) FROM produto;
+SELECT ROUND(AVG(preco),2) FROM produto;
+-- r) Exibir a quantidade de preços acima da média entre todos os produtos;
+SELECT COUNT(preco) from produto 
+	WHERE preco > (SELECT AVG(preco) FROM produto);
+-- s) Exibir a soma dos preços distintos dos produtos;
+SELECT SUM(preco) FROM produto
+	WHERE nome = 'Detergente';
+SELECT SUM(preco) FROM produto
+	WHERE nome = 'Sabonete';
+SELECT SUM(preco) FROM produto
+	WHERE nome = 'Alface';
+SELECT SUM(preco) FROM produto
+	WHERE nome = 'Guarana';
+SELECT SUM(preco) FROM produto
+	WHERE nome = 'Red bull';
+SELECT SUM(preco) FROM produto
+	WHERE nome = 'Pao';
+
+-- t) Exibir a soma dos preços dos produtos agrupado por uma determinada venda;
+SELECT SUM(produto.preco), venda.fkProduto 
+ FROM produto 
+ JOIN venda
+	ON produto.idProduto = venda.fkProduto
+	GROUP BY fkProduto;
+	
